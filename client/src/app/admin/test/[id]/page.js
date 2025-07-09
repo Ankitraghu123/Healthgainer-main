2540d11909788c7ef8e23da5d6cb39f5187bb43b
webwithastha <lodhiastha85@gmail.com>
2025-07-09 11:59:42 +0700

    test 3

--- client/src/app/admin/test/[id]/page.js
+++ client/src/app/admin/test/[id]/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = ({params}) => {
   return (
-    <div>page dynamic id updated : {params.id}</div>
+    <div>page dynamic id updated 3.0 : {params.id}</div>
   )
 }
 
--- client/src/app/admin/test/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>test 2.0</div>
-  )
-}
-
-export default page
