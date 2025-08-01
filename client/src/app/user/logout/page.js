f725ce8a9e233e21222ef8d47ae34f5491c873b3
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2025-08-01 15:42:14 +0530

    update sb code

--- client/src/app/user/logout/page.js
+++ client/src/app/user/logout/page.js
@@ -4,36 +4,32 @@ import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { logout } from "@/redux/slices/authSlice";
 // import { loadUserFromStorage } from '@/redux/slices/authSlice';
-import { clearUser } from '@/redux/slices/authSlice';
-
+import { clearUser } from "@/redux/slices/authSlice";
 
 const Logout = () => {
-    const router = useRouter();
-    const dispatch = useDispatch();
-   
+  const router = useRouter();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const handleLogout = async () => {
-            try {
-                await dispatch(logout()).unwrap(); // ✅ Ensure Redux state clears
-                // dispatch(loadUserFromStorage());
-                dispatch(clearUser());
+  useEffect(() => {
+    const handleLogout = async () => {
+      try {
+        await dispatch(logout()).unwrap();
+        dispatch(clearUser());
+        window.location.href = "/login";
+        // setTimeout(() => router.push("/login"), 500);
+      } catch (error) {
+        console.error("Error during logout:", error);
+      }
+    };
 
-                // Navigate after clearing everything
-                setTimeout(() => router.push("/login"), 500);
-            } catch (error) {
-                console.error("Error during logout:", error);
-            }
-        };
+    handleLogout();
+  }, [dispatch, router]);
 
-        handleLogout();
-    }, [dispatch, router]);
-
-    return (
-        <div className="flex items-center justify-center min-h-80 bg-gray-100">
-            <h1 className="text-2xl font-semibold">Logging out...</h1>
-        </div>
-    );
+  return (
+    <div className='flex items-center justify-center min-h-80 bg-gray-100'>
+      <h1 className='text-2xl font-semibold'>Logging out...</h1>
+    </div>
+  );
 };
 
 export default Logout;
