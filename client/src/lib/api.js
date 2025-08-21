8b2cba1dfcc09814bc42aec43271ea6ebe00015a
Siddhant sahu <114759848+sidddsahu@users.noreply.github.com>
2025-08-21 16:08:23 +0530

    first commit

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
 const API = axios.create({
-  // baseURL: "https://healthgainer-main.onrender.com/api/v1", // Fallback base URL for local development
+  baseURL: "https://backend.codepact.in/api/v1", // Fallback base URL for local development
 
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
