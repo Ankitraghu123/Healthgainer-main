ab96b9583844b8e40fd84b13d709dbb1997e1db5
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2025-09-03 18:05:01 +0530

    update code

--- server/models/cartModel.js
+++ server/models/cartModel.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const cartItemSchema = new mongoose.Schema({
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }, 
-  variantId: { type: mongoose.Schema.Types.ObjectId, required: true }, // Stores selected variant
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  variantId: { type: mongoose.Schema.Types.ObjectId, required: false }, // Stores selected variant
   name: { type: String, required: true }, // Product Name
   weight: { type: String, required: true }, // Variant weight
   price: { type: Number, required: true }, // Discounted price of the variant
@@ -10,17 +14,21 @@ const cartItemSchema = new mongoose.Schema({
   discount: { type: Number, default: 0 }, // Discount in percentage
   quantity: { type: Number, required: true, default: 1 },
   subtotal: { type: Number, required: true }, // price * quantity
-  images: [{ type: String, required: true }] // Variant images
+  images: [{ type: String, required: true }], // Variant images
 });
 
 const cartSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to User
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }, // Reference to User
     items: [cartItemSchema], // Array of products in the cart
     totalAmount: { type: Number, required: true, default: 0 }, // Total cart value
     totalItems: { type: Number, required: true, default: 0 }, // Total number of unique items
     createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    updatedAt: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
